4a4b81e982f8e2d0c63b022e49e0170d1ac75e6c
Rohit Nair P <rohitnairmuttathethu@gmail.com>
2026-04-21 13:10:12 +0530

    feat: scaffold header component structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="header" id="app-header">
+      <div className="header__inner">
+        <div className="header__logo">
+          <img src="/logo.svg" alt="KoinX" className="koinx-logo-img" />
+        </div>
+      </div>
+    </header>
+  );
+}
